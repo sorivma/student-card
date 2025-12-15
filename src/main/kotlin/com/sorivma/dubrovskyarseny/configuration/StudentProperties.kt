776e9d4788705744480a80a9695d546fe74f2e0a
sorivma <sorivma03.06@gmail.com>
2025-12-15 16:59:09 +0300

    - add links for labels

--- src/main/kotlin/com/sorivma/dubrovskyarseny/configuration/StudentProperties.kt
+++ src/main/kotlin/com/sorivma/dubrovskyarseny/configuration/StudentProperties.kt
@@ -5,6 +5,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 @ConfigurationProperties(prefix ="student")
 data class StudentProperties(
     var fullName: String = "",
+    var fullNameLink: String? = null,
     var group: String = "",
+    var groupLink: String? = null,
     var institute: String = "",
+    var instituteLink: String? = null
 )
