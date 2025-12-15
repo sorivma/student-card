001755df0f825a3aed2eba4a26d23fd050935b7e
sorivma <sorivma03.06@gmail.com>
2025-12-15 17:08:45 +0300

    Revert "- add links for labels"
    This reverts commit 776e9d4788705744480a80a9695d546fe74f2e0a.

--- src/main/kotlin/com/sorivma/dubrovskyarseny/configuration/StudentProperties.kt
+++ src/main/kotlin/com/sorivma/dubrovskyarseny/configuration/StudentProperties.kt
@@ -5,9 +5,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 @ConfigurationProperties(prefix ="student")
 data class StudentProperties(
     var fullName: String = "",
-    var fullNameLink: String? = null,
     var group: String = "",
-    var groupLink: String? = null,
     var institute: String = "",
-    var instituteLink: String? = null
 )
